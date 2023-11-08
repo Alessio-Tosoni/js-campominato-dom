3be69340e7845b838f77eaf595ad04162cc914e0
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-08 21:33:33 +0100

    aggiunge bombe

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ generator.addEventListener("click", function(){
         let numcelle = 100;
         let grid = document.getElementById("grid")
         const random = []
+
         for (let i = 0; i < 16; i++) {
-            random.push(Math.floor(Math.random() * numcelle) +1)
-            console.log(random)
+           z = (Math.floor(Math.random() * numcelle) +1)
+           console.log(z)
+           if (random.includes(z)){
+               i--
+            } else{
+                random.push(z)
+            }
         }
+       
         for (let i = 1; i <= numcelle; i++ ){
             const cella = document.createElement("div")
             cella.classList.add("square-facile");
@@ -21,8 +28,11 @@ generator.addEventListener("click", function(){
                 cella.classList.toggle("evidenziata")
                //devo comparare il risultato di random(che è un array) con i di cella che clicco
                
-               if (i == random){
+               if (random.includes(i)){
                 cella.classList.add("bomba")
+                alert("hai perso")
+                //provo a fermare il gioco
+                
                }
             })
             
@@ -33,6 +43,18 @@ generator.addEventListener("click", function(){
     } else if (difficult == "medio"){
         let numcelle = 81;
         let grid = document.getElementById("grid")
+        const random = []
+
+        for (let i = 0; i < 16; i++) {
+           z = (Math.floor(Math.random() * numcelle) +1)
+           console.log(z)
+           if (random.includes(z)){
+               i--
+            } else{
+                random.push(z)
+            }
+        }
+
         for (let i = 1; i <= numcelle; i++ ){
             const cella = document.createElement("div")
             cella.classList.add("square-medio");
@@ -42,12 +64,28 @@ generator.addEventListener("click", function(){
             cella.addEventListener("click", function(){
                 console.log("cella cliccata", i)
                 cella.classList.toggle("evidenziata")
+                if (random.includes(i)){
+                    cella.classList.add("bomba")
+                    alert("hai perso")
+                    //provo a fermare il gioco
+                    
+                }
             })
             //generatore bombe
         }
     } else{
         let numcelle = 49;
         let grid = document.getElementById("grid")
+        const random = []
+        for (let i = 0; i < 16; i++) {
+            z = (Math.floor(Math.random() * numcelle) +1)
+            console.log(z)
+            if (random.includes(z)){
+                i--
+            } else{
+                 random.push(z)
+            }
+        }
         for (let i = 1; i <= numcelle; i++ ){
             const cella = document.createElement("div")
             cella.classList.add("square-difficile");
@@ -57,8 +95,14 @@ generator.addEventListener("click", function(){
             cella.addEventListener("click", function(){
                 console.log("cella cliccata", i)
                 cella.classList.toggle("evidenziata")
+                //generatore bombe
+                if (random.includes(i)){
+                    cella.classList.add("bomba")
+                    alert("hai perso")
+                    //provo a fermare il gioco
+                    
+                }
             })
-            //generatore bombe
         }
 
     }
